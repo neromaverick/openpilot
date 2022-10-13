0311dc03635db2453c828b398941553f53eabdc9
hoomoose <94947902+hoomoose@users.noreply.github.com>
2022-10-13 11:37:52 -0600

    Cruise Buttons bus 6

--- selfdrive/car/hyundai/hyundaicanfd.py
+++ selfdrive/car/hyundai/hyundaicanfd.py
@@ -45,7 +45,7 @@ def create_buttons(packer, cnt, btn):
     "SET_ME_1": 1,
     "CRUISE_BUTTONS": btn,
   }
-  return packer.make_can_msg("CRUISE_BUTTONS", 5, values)
+  return packer.make_can_msg("CRUISE_BUTTONS", 6, values)
 
 def create_acc_cancel(packer, CP, cruise_info_copy):
   values = cruise_info_copy
