bded07ba98afd0803573bd602f79c16f5e0844b5
hoomoose <94947902+hoomoose@users.noreply.github.com>
2022-10-13 11:09:30 -0600

    Send 5 Resume Button Presses

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -128,7 +128,7 @@ class CarController:
           elif CC.cruiseControl.resume:
             if not (self.CP.flags & HyundaiFlags.CANFD_ALT_BUTTONS):
               for _ in range(20):
-                can_sends.append(hyundaicanfd.create_buttons(self.packer, CS.buttons_counter+1, Buttons.RES_ACCEL))
+                can_sends.extend([hyundaicanfd.create_buttons(self.packer, CS.buttons_counter+1, Buttons.RES_ACCEL)] * 5)
               self.last_button_frame = self.frame
     else:
       # Count up to MAX_ANGLE_FRAMES, at which point we need to cut torque to avoid a steering fault
